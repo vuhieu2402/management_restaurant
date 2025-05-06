2a30b0d9315648660dd342e4dd5ba1de81b03fe5
vuhieu2402 <vuhieu24022002h@gmail.com>
2025-05-06 22:14:39 +0700

    upload search

--- backend/manage_restaurant/home/views.py
+++ backend/manage_restaurant/home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import  viewsets
+from rest_framework import viewsets, filters
 from .models import Category, Dish
 from .serializers import CategorySerializer, DishSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -15,3 +15,5 @@ class DishViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
     queryset = Dish.objects.all()
     serializer_class = DishSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'description']
